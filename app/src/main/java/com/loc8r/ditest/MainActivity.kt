da22669e1e6113c373fd138e83352d3d2f104c76
szaske <github@zaske.com>
2018-08-12 18:07:58 -0700

    STEP #1. An extremely simple Android Application

---
+++ app/src/main/java/com/loc8r/ditest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.loc8r.ditest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * STEP #1. An extremely simple Android Application
+     *
+     * This repo was created to help me learn and understand Dagger2 and Kotlin.  I'll leave it here
+     * to help others too.  YOu can walk through each step by checking out different branches.
+     *
+     * This Project contains one activity and one separate class, that will need to be injected
+     * called SecretInfo
+     */
+    val info = SecretInfo()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        text_info.text = info.cool_dude
+    }
+}
---
+++ app/src/main/java/com/loc8r/ditest/SecretInfo.kt
@@ -0,0 +1,5 @@
+package com.loc8r.ditest
+
+class SecretInfo {
+    val cool_dude = "Steve Zaske"
+}
