83b4daffb7f08fbca60e3c622d26fe9e74c243e8
szaske <github@zaske.com>
2018-08-13 13:39:38 -0700

    STEP #5. Alter your class to accept the dependency injections

--- app/src/main/java/com/loc8r/ditest/MainActivity.kt
+++ app/src/main/java/com/loc8r/ditest/MainActivity.kt
@@ -2,7 +2,9 @@ package com.loc8r.ditest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.loc8r.ditest.di.DaggerAppComponent
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +29,12 @@ class MainActivity : AppCompatActivity() {
      * STEP #4. Configuring injectable classes.  We need to tell Dagger2 what classes can be
      * injected.  We do that by adding the @Inject annotation to the class.
      *
+     * STEP #5. Alter your class to accept the dependency injections.
+     *       5a. Change injected properties/fields to  @Inject lateinit var's
+     *       5b. Inject using the Dagger created call (see line 61 below)
+     *
+     * Now this example is complete.  We've removed the initialization of a dependency and instead
+     * injected it using Dagger2
      */
 
 
@@ -39,12 +47,21 @@ class MainActivity : AppCompatActivity() {
      * not.  In this simple example 'Separation of concerns' is overkill, but it's good practice
      * to follow it on professional projects.
      */
-    val info = SecretInfo()
+    // Step #5 was -> val info = SecretInfo(). lateinit allows us to compile without causing an error
+    // because the non-null variable has not been initialize in the constructor.
+    @Inject lateinit var info: SecretInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        /**
+         * Now we tell Dagger to inject, using the interface we created by called
+         * DaggerAppComponent.create().  Wait, what?  Where did this DaggerAppComponent class come
+         * from?  IMO this is the most confusing part about Dagger2.  Dagger creates this class as
+         * part of it's magic, and when you type this line Android Studio code-completion will not
+         * recognize it until after you build or rebuild the project.
+         */
+        DaggerAppComponent.create().inject(this)
         text_info.text = info.cool_dude
     }
 }
